4c016939f806ba11e777842547a7f93ddc369a20
franranieli <franranieli@gmail.com>
2022-11-15 23:57:07 -0300

    validaciones frontend ususario

---
+++ public/js/validacionesUsuario.js
@@ -0,0 +1,56 @@
+window.onload = function () {
+   
+    let formulario = document.querySelector("form.formSignup");
+
+    let errors = [];
+  
+    let name = document.querySelector(".name");
+    let email = document.querySelector(".email")
+    let adress = document.querySelector(".adress");
+    let password = document.querySelector(".password");
+    let pass_confirm = document.querySelector(".pass_confirm");
+    let img = document.querySelector(".img");
+  
+  
+    formulario.addEventListener("submit", (e) => {
+      errors = [];
+  
+        if(name.value == ""){
+            errors.push("El nombre es obligatorio");
+        }else if(name.value.length < 2){
+            errors.push("El nombre debe tener mas de 2 caracteres");
+        }
+
+        let validRegex = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/;
+
+
+        if(email.value == ""){
+            errors.push("El email es obligatorio");
+        }else if(name.email.match(validRegex)){
+            errors.push("El email debe valido");
+        }
+
+        if(password.value == ""){
+            errors.push("La contraseña es obligatoria");
+        }else if(password.value.length < 8){
+            errors.push("La contraseña debe tener mas de 7 caracteres");
+        }
+
+        if(pass_confirm.value != password.value){
+            errors.push("Las contraseñas deben ser iguales");
+        }
+
+  
+      if (errors.length > 0) {
+        e.preventDefault();
+        let ulErrors = document.querySelector(".errores");
+        
+        for (let i = 0; i < errors.length; i++) {
+          ulErrors.innerHTML += `<li >  ${errors[i]} </li>`; 
+        }
+      } else {
+        alert("La validación fué exitosa");
+        form.submit();
+      }
+    });
+  };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require("multer");
+const { body } = require("express-validator");
 
 const indexController = require ("../controller/indexController");
 
@@ -47,14 +48,14 @@ router.delete("/product-detail/:id", indexController.delete)
 
 //muestra el crear producto
 router.get("/crea",indexController.crea);
-router.post("/indexRouter/crea", upload.any(), indexController.store);
+router.post("/indexRouter/crea", upload.any(), validaciones, indexController.store);
 // router.post("/indexRouter/crea", upload.single("imageBack"), indexController.store);
 
 
 
 //muestra el editar producto
 router.get("/edit/:id",indexController.edit);
-router.put("/edit/:id", upload.any("imageFrente","imageBack"), indexController.update);
+router.put("/edit/:id", upload.any("imageFrente","imageBack"), validaciones, indexController.update);
 // router.put("/edit/:id", upload.single("imageBack"), indexController.update);
 
 
